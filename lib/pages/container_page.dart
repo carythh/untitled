b8aff8a48205f5a1f2d427b67cd4091f522b54fe
tan huohui <carythh@users.noreply.github.com>
2022-10-13 09:12:18 +0800

    newProject
    a flutter demo

---
+++ lib/pages/container_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+// import 'package:untitled/pages/group/group_page.dart';
+// import 'package:untitled/pages/movie/book_audio_video_page.dart';
+// import 'package:untitled/pages/home/home_page.dart';
+// import 'package:untitled/pages/person/person_center_page.dart';
+// import 'package:untitled/pages/shop_page.dart';
+
+
+///这个页面是作为整个APP的最外层的容器，以Tab为基础控制每个item的显示与隐藏
+class ContainerPage extends StatefulWidget {
+   ContainerPage({ Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ContainerPageState();
+  }
+  
+  
+}
+
+class _ContainerPageState extends State<ContainerPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
